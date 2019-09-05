54db12ac236b54ad5f310cfeb4da836485f9f0ce
Hadziq 哈明飛 <hadziq@gmail.com>
2019-09-05 09:10:18 +0700

    Berhasil memodulasi fungsi cek WebGL

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 (function() {
 
   var canvas = document.getElementById("glcanvas");
-  var gl;
-  var contexts = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
-
-  for (var i = 0; i < contexts.length; i++) {
-    try {
-      var context = contexts[i];
-      gl = canvas.getContext(context);
-    } catch (error) {
-      // Sementara kosong
-    }
-    if (gl) {
-      break;
-    }
-  }
-  if (!gl) {
-    alert("WebGL tidak ditemukan. Tolong gunakan Chrome/Firefox terbaru.");
-  }
+  var gl = glUtils.checkWebGL(canvas);
 
   // Bersihkan layar jadi hitam
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(1.0, 0.0, 0.0, 1.0);
 
   // Bersihkan buffernya canvas
   gl.clear(gl.COLOR_BUFFER_BIT);
---
+++ libs/glUtils.js
@@ -0,0 +1,28 @@
+(function(global) {
+
+  var glUtils = {
+    VERSION: '0.0.1',
+    checkWebGL: function(canvas) {
+      var gl;
+      var contexts = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+
+      for (var i = 0; i < contexts.length; i++) {
+        try {
+          var context = contexts[i];
+          gl = canvas.getContext(context);
+        } catch (error) {
+          // Sementara kosong
+        }
+        if (gl) {
+          break;
+        }
+      }
+      if (!gl) {
+        alert("WebGL tidak ditemukan. Tolong gunakan Chrome/Firefox terbaru.");
+      }
+      return gl;
+    }
+  };
+  global.glUtils = glUtils;
+
+})(window || this);
